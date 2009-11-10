77bab48b017687ccae9e8bd870c8abe572f94b65
Joshua Anyan <josh.anyan@announcemedia.com>
2009-11-10 07:32:02 -0600

    #6 complete

---
+++ 6.rb
@@ -0,0 +1,25 @@
+def check(num,max)
+	max.downto(1) { |i|
+		if num%i != 0
+			return false
+		end
+	}
+	return true
+end
+
+puts "find natural differences from 1 to..."
+number = gets.chomp.to_i
+sum_square = 0
+square_sum = 0
+
+1.upto(number) { |i|
+	sum_square += i**2
+	square_sum += i
+}
+
+difference = square_sum**2 - sum_square
+
+puts ''
+puts difference
+command = "growlnotify -t '"+$0.to_s+"' -m '"+difference.to_s+"'"
+exec(command)
