a0b151b34e92269b105a4636a313a539f5a17e9f
Joshua Anyan <josh.anyan@announcemedia.com>
2009-11-10 01:31:58 -0600

    added some user input functionality for #3

--- 3.rb
+++ 3.rb
@@ -1,19 +1,23 @@
-original_num = 13195
+#original_num = 600851475143
+puts "please enter a number to factor"
+original_num = gets.chomp.to_i
 num = original_num
 done = false
 pointer = 2
 list = Array.new
 
 while !done
-	puts pointer
 	if(num%pointer == 0)
 		list = list << pointer
-		puts 'added',pointer
-		num = num%pointer
-		puts 'modified num',num
+		num = num/pointer
+		if(num == 1)
+			done = true
+		end
 		pointer = 2
+		sleep 0.2
 	else
 		pointer += 1
 	end
 end
+puts "\nyour factors are:"
 puts list
